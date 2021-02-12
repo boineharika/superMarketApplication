4944c892d26fb5fc550a19e76a6c3e3b5ce7261c
Harika <harikahari446@gmail.com>
2021-02-11 20:35:19 -0500

    New project setup for fresh market

---
+++ freshmarket/src/test/java/com/learning/supermarket/freshmarket/FreshmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.supermarket.freshmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
