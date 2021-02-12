51bf2623597b8c704502c0f6ed091971d5a31e1c
Harika <harikahari446@gmail.com>
2021-02-11 20:40:32 -0500

    updated application

--- freshmarket/src/main/java/com/learning/supermarket/freshmarket/FreshmarketApplication.java
+++ freshmarket/src/main/java/com/learning/supermarket/freshmarket/FreshmarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreshmarketApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Applicationn boot started");
 		SpringApplication.run(FreshmarketApplication.class, args);
 	}
 
